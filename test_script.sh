922b8af2cf23b6629ef8cc759881c80bd79fa776
Your Name <you@example.com>
2020-06-04 17:55:32 +0300

    Added one more line from local repo

--- test_script.sh
+++ test_script.sh
@@ -4,5 +4,6 @@ echo "==================================="
 echo "This code is from GitHub repository"
 echo "The reponame is $REPONAME"
 echo "And now it was triggered by GitSCM commit"
+echo "One more line"
 echo "==================================="
 

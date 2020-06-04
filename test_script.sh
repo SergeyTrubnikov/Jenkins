693797832042fa113050e0b73cfe57d0b8ffd439
SergeyTrubnikov <49154831+SergeyTrubnikov@users.noreply.github.com>
2020-06-04 17:35:05 +0300

    Update test_script.sh
    Trigger by commit

--- test_script.sh
+++ test_script.sh
@@ -3,4 +3,6 @@
 echo "==================================="
 echo "This code is from GitHub repository"
 echo "The reponame is $REPONAME"
+echo "And now it was triggered by GitSCM commit"
 echo "==================================="
+

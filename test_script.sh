93ffab35a7a56bfe1f3120b33f5178814468caa4
SergeyTrubnikov <49154831+SergeyTrubnikov@users.noreply.github.com>
2020-06-04 22:11:15 +0300

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -5,5 +5,6 @@ echo "This code is from GitHub repository"
 echo "The reponame is $REPONAME"
 echo "And now it was triggered by GitSCM commit"
 echo "One more line"
+echo "Test with cloud VM"
 echo "==================================="
 
